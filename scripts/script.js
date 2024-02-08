aba27da2a863ff4575d78b938a7f6b3b633d300a
Davíð Berman <davidjberman@gmail.com>
2024-02-08 02:54:12 +0000

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,8 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://raw.githubusercontent.com/davidjrb/md2html/master/README.md')
-      .then(response => response.text())
-      .then(text => {
-        document.getElementById('markdownContent').innerHTML = marked.parse(text);
-      })
-      .catch(err => console.error('Fetch error:', err));
-  });
-  
+  fetch('https://raw.githubusercontent.com/davidjrb/md2html/master/README.md')
+    .then(response => response.text())
+    .then(markdown => {
+      document.getElementById('markdownContent').innerHTML = marked.parse(markdown);
+    })
+    .catch(error => console.error('Error loading Markdown:', error));
+});
