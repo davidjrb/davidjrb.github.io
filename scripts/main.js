a1ccf07f3871606aea859673aeca806f842e01bd
Davíð Berman <davidjberman@gmail.com>
2024-02-08 02:35:58 +0000

    script added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://raw.githubusercontent.com/davidjrb/md2html/master/README.md')
+      .then(response => response.text())
+      .then(text => {
+        document.getElementById('markdownContent').innerHTML = marked.parse(text);
+      })
+      .catch(err => console.error('Fetch error:', err));
+  });
+  
